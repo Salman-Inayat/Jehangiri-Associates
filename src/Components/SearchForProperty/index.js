a2553244de6a76da0659a28f4624a36b1a6735d9
Salman-Inayat <salmaninayat1999@gmail.com>
2021-04-23 16:26:07 +0500

    Components files added

---
+++ src/Components/SearchForProperty/index.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import './style.css'
+import {Grid} from '@material-ui/core'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+
+
+AOS.init({})
+
+function SearchProperty(){
+    let cities = [ 'Karachi', 'Lahore', 'Faisalābād', 'Rāwalpindi', 'Multān', 'Gujrānwāla', 'Peshāwar', 'Abbottābād', 'Islamabad', 'Quetta', 'Sargodha']
+    const [value, setValue] = React.useState({
+        PropID: null,
+        BoR: '',
+        City: '',
+        PropType: '',
+        MinPr: null,
+        MaxPr: null,
+    })
+    const handleChange =(event) => {
+        const { name, value} = event.target;
+
+        setValue((state) => ({
+            ...state,
+            [name]: value
+        }))
+    }
+
+    return(
+        <div className='search-cmp'>
+            <div className="search-title" data-aos="zoom-in-up">
+                <h3>Find your dream home</h3>
+            </div>
+            <div className="search-box" data-aos="zoom-in-up">
+            <Grid container spacing={5}>
+                <Grid items md={6} xs={12} data-aos="zoom-in">
+                <input 
+                    type="number" 
+                    placeholder="Property ID" 
+                    name='PropID' 
+                    className="searchInputSoP" 
+                    onChange={handleChange}
+                    value={value["PropID"]}
+                    
+                />
+                </Grid>
+                <Grid items md={6} xs={12} data-aos="zoom-in">
+                <select label="Buy Or Rent" className="searchInputSoP" name='BoR' value={value["BoR"]} onChange={handleChange}>
+                    <option value="Rent" className="searchOption">
+                        Rent
+                    </option>
+                    <option value="Buy" className="searchOption">
+                        Buy
+                    </option>
+                </select>
+                </Grid>
+                <Grid items md={6} xs={12} data-aos="zoom-in">
+                <select label="Select Your City" className="searchInputSoP" name='City' value={value["City"]} onChange={handleChange}>
+                    {cities.map(city => (
+                        <option value={city}>
+                            {city}
+                        </option>
+                    ))}
+                </select>
+                </Grid>
+                <Grid items md={6} xs={12} data-aos="zoom-in">
+                <select label="Select Property Type" className="searchInputSoP" name='PropType' value={value["PropType"]} onChange={handleChange}>
+                    <option value="Home" className="searchOption">
+                        Home
+                    </option>
+                    <option value="Plot" className="searchOption">
+                        Plot
+                    </option>
+                    <option value="Commercial" className="searchOption">
+                        Commercial
+                    </option>
+                </select> 
+                </Grid>
+                <Grid items md={6} xs={12} data-aos="zoom-in">
+                <input type="number" placeholder="Minimum Price PKR" className="searchInputSoP" name='MinPr' value={value["MinPr"]} onChange={handleChange}/>
+
+                </Grid>
+                <Grid items md={6} xs={12} data-aos="zoom-in">
+                <input type="number" placeholder="Maximum Price PKR" className="searchInputSoP" name='MaxPr' value={value["MaxPr"]} onChange={handleChange}/>
+
+                </Grid>
+                <button className="search-button" type="button">
+                    Search Now
+                </button>
+            </Grid>  
+            </div>
+        </div>
+    )
+}
+
+export default SearchProperty
